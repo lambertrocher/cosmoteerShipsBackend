f87497d5694b7d246e52da98d71d6c680d1e6ce2
lambert <lambert.rocher@jellysmack.com>
2023-01-09 01:06:00 +0100

    (wip) add login with discord

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BlueprintsController } from './blueprints/blueprints.controller';
 import { BlueprintsService } from './blueprints/blueprints.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [BlueprintsController],
   providers: [BlueprintsService],
 })
