e7c49dd4dfee222fa1ec85f5d77e16a76ce7839a
lambert <lambert.rocher@jellysmack.com>
2023-01-09 19:22:57 +0100

    move auth to service

--- src/blueprints/blueprints.controller.ts
+++ src/blueprints/blueprints.controller.ts
@@ -2,42 +2,16 @@ import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { BlueprintsService } from './blueprints.service';
 import { Blueprint } from './interfaces/blueprint.interface';
 import { CreateBlueprintDto } from './dto/create-blueprint.dto';
-import axios from 'axios';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('blueprints')
 export class BlueprintsController {
-  constructor(
-    private blueprintsService: BlueprintsService,
-    private configService: ConfigService,
-  ) {}
-
-  async getDiscordToken(code: string) {
-    const discord_base_url = this.configService.get<string>('DISCORD_BASE_URL');
-    const discord_token_url = `${discord_base_url}/oauth2/token`;
-    const client_id = this.configService.get<string>('CLIENT_ID');
-    const client_secret = this.configService.get<string>('CLIENT_SECRET');
-    const redirect_url = this.configService.get<string>('REDIRECT_URL');
-
-    const formData = new URLSearchParams();
-    formData.append('grant_type', 'authorization_code');
-    formData.append('code', code);
-    formData.append('client_id', client_id);
-    formData.append('client_secret', client_secret);
-    formData.append('redirect_uri', redirect_url);
-
-    try {
-      const response = await axios.post(discord_token_url, formData);
-      return response.data.access_token;
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  constructor(private blueprintsService: BlueprintsService) {}
 
   @Get('/login')
   async login(@Query('code') code: string): Promise<string> {
     console.log('calling login route');
-    return await this.getDiscordToken(code);
+    const discordToken = await this.blueprintsService.getDiscordToken(code);
+    return await this.blueprintsService.verifyDiscordToken(discordToken);
   }
 
   @Get(':id')
